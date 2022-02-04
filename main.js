18c79c13e88e6eee2140b9d28912c93b5d73d31a
Zak Espley <zespley@physics.ucsb.edu>
2022-02-03 17:30:45 -0800

    Built basic interface for socket testing.

---
+++ main.js
@@ -0,0 +1,16 @@
+let header = document.getElementById("header");
+let textInput = document.getElementById("wsCmd")
+
+//Create Websocket and connect to raspberry pi
+// Something like ws://<pi ip address>:8081
+//
+
+// When text is entered and the "Enter" key is hit do something with the value.
+// Maybe just have the raspberrry pi print the text.
+textInput.addEventListener("keyup", function(event) {
+    if (event.key === "Enter") {
+        //When enter key goes up send the text
+        // to the pi.
+        header.textContent=event.target.value;
+    }
+})
