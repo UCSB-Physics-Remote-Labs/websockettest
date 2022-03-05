418420b9da8bf591860fd5368cbf938fe8ecb0c4
ciortiz <ciortiz@ucsb.edu>
2022-03-04 17:03:22 -0800

    edit3

--- ws.py
+++ ws.py
@@ -7,10 +7,6 @@ port = 80
 
 Handler = http.server.SimpleHTTPRequestHandler
 
-with socketserver.TCPServer(("", port), Handler) as httpd:
-    print("Serving at port", port)
-    httpd.serve_forever()
-
 async def server(websocket):
     async for message in websocket:
         await websocket.send(f'You told me: {message}')
@@ -19,3 +15,7 @@ async def main():
     async with websockets.serve(server, "0.0.0.0", 6048):
         await asyncio.Future()
 asyncio.run(main())
+
+with socketserver.TCPServer(("", port), Handler) as httpd:
+    print("Serving at port", port)
+    httpd.serve_forever()
